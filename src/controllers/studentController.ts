a6182d95040c7ea34131dfccc352a685d9f94b14
agartanah <vyatin2017@mail.ru>
2025-02-04 20:17:46 +0700

    add getting students in controller

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { Student } from "../models/student";
 
-const getStudents = async (req: Request, res: Response) => {};
+const getStudents = async (req: Request, res: Response) => {
+  try {
+    const students = await Student.find();
+    res.status(200).json(students);
+  } catch (error) {
+    res.status(500).send("Something went wrong on server " + error);
+  }
+};
 
-const getStudent = async (req: Request, res: Response) => {};
+// const getStudent = async (req: Request, res: Response) => {};
 
-const deleteStudent = async (req: Request, res: Response) => {};
+// const deleteStudent = async (req: Request, res: Response) => {};
 
-export { getStudents, getStudent, deleteStudent };
+export { getStudents };
