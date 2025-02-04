14c98fb596f2bcf78aa863c34ff28054e8615c8b
agartanah <vyatin2017@mail.ru>
2025-02-04 19:16:54 +0700

    add templates for controllers

---
+++ src/controllers/enseignantController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Teacher } from "../models/teacher";
+
+const getTeachers = async (req: Request, res: Response) => {};
+
+const getTeacher = async (req: Request, res: Response) => {};
+
+const deleteTeacher = async (req: Request, res: Response) => {};
+
+export { getTeachers, getTeacher, deleteTeacher };
---
+++ src/controllers/studentController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Student } from "../models/student";
+
+const getStudents = async (req: Request, res: Response) => {};
+
+const getStudent = async (req: Request, res: Response) => {};
+
+const deleteStudent = async (req: Request, res: Response) => {};
+
+export { getStudents, getStudent, deleteStudent };
