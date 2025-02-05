05a651553933f1b70380c80305871eea4ed816f0
agartanah <vyatin2017@mail.ru>
2025-02-05 23:09:32 +0700

    remove hbs from view engine. add two routers: main '/' and authorization '/auth'

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,35 @@ import { connectDB } from "./config/db";
 import { env } from "process";
 import { studentRoutes } from "./routes/studentRoutes";
 import { teacherRoutes } from "./routes/teacherRoutes";
-import path from "path";
-import hbs from "hbs";
+import { authorisationRoutes } from "./routes/authorisationRoutes";
+import { profileRoutes } from "./routes/profileRoutes";
 
 const app = express();
 const port = env.port;
 
 connectDB();
 
-const viewsPath = path.join(__dirname, "views");
-const partialsPath = path.join(__dirname, "views/partials");
+// const viewsPath = path.join(__dirname, "view");
+// const partialsPath = path.join(__dirname, "view/partials");
 
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
+// app.engine(
+//   "hbs",
+//   engine({
+//     layoutsDir: path.join(viewsPath, "layouts"),
+//     defaultLayout: "layout",
+//     extname: "hbs",
+//   }),
+// );
+// app.set("view engine", "hbs");
+// app.set("views", viewsPath);
+// hbs.registerPartials(partialsPath);
+
+// app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.json());
+
+app.use("/", profileRoutes);
+app.use("/auth", authorisationRoutes);
 app.use("/student", studentRoutes);
 app.use("/teacher", teacherRoutes);
 
