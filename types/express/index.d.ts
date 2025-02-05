8769bcdf0041828c281fdd61037f3db4e3fe0884
agartanah <vyatin2017@mail.ru>
2025-02-06 00:55:58 +0700

    for ext field in Request

---
+++ types/express/index.d.ts
@@ -0,0 +1,21 @@
+// declare module "express-serve-static-core" {
+//   interface Request {
+//     user?: {
+//       userId: string;
+//       role: "student" | "teacher";
+//     };
+//   }
+// }
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: {
+//         userId: string;
+//         role: "student" | "teacher";
+//       };
+//     }
+//   }
+// }
+
+// export {}; // ВАЖНО!
