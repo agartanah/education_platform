1458c29ed8368538372c66ccc1c31b2fba194e29
agartanah <vyatin2017@mail.ru>
2025-03-04 18:00:52 +0700

    remove comments, add routes list

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,18 @@
 import express from "express";
 import { connectDB } from "./config/db";
 import { env } from "process";
-import { studentRoutes } from "./routes/studentRoutes";
-import { teacherRoutes } from "./routes/teacherRoutes";
-import { authorisationRoutes } from "./routes/authorisationRoutes";
-import { profileRoutes } from "./routes/profileRoutes";
+import routes from "./routes/routes";
 
 const app = express();
 const port = env.port;
 
 connectDB();
 
-// const viewsPath = path.join(__dirname, "view");
-// const partialsPath = path.join(__dirname, "view/partials");
-
-// app.engine(
-//   "hbs",
-//   engine({
-//     layoutsDir: path.join(viewsPath, "layouts"),
-//     defaultLayout: "layout",
-//     extname: "hbs",
-//   }),
-// );
-// app.set("view engine", "hbs");
-// app.set("views", viewsPath);
-// hbs.registerPartials(partialsPath);
-
-// app.use(express.static(path.join(__dirname, "../public")));
-
 app.use(express.json());
 
-app.use("/", profileRoutes);
-app.use("/auth", authorisationRoutes);
-app.use("/student", studentRoutes);
-app.use("/teacher", teacherRoutes);
-
-// createUsers();
+routes.forEach((route) => {
+  app.use(route.name, route.router);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
