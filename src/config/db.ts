0ed6e75b8c3c6cf2b4349047c2b2c91ac3754847
agartanah <vyatin2017@mail.ru>
2025-03-04 17:49:35 +0700

    remove second catch

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,17 +2,13 @@ import mongoose from "mongoose";
 import { env } from "./env";
 
 export const connectDB = async () => {
-  try {
-    await mongoose
-      .connect(env.mongoUri)
-      .then(() => {
-        console.log("Подключение к бд произошло успешно");
-      })
-      .catch((err) => {
-        console.error("Ошибка подключения:", err);
-      });
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  }
+  await mongoose
+    .connect(env.mongoUri)
+    .then(() => {
+      console.log("Подключение к бд произошло успешно");
+    })
+    .catch((err) => {
+      console.error("Ошибка подключения:", err);
+      process.exit(1);
+    });
 };
