9a1067f7ba301c1fc99c3799179a3e080ecef83d
agartanah <vyatin2017@mail.ru>
2025-05-07 01:28:19 +0700

    create error middleware for server error

---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { ZodError } from 'zod';
+
+interface CustomError extends Error {
+  status?: number;
+}
+
+export const errorMiddleware = (
+  err: CustomError,
+  req: Request,
+  res: Response,
+): void => {
+  if (err instanceof ZodError) {
+    res.status(400).json({ message: 'Ошибка валидации', errors: err.errors });
+    return;
+  }
+
+  const statusCode = err.status || 500;
+  const message = err.message || 'Внутренняя ошибка сервера';
+
+  res.status(statusCode).json({ message });
+};
