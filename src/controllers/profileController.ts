994cbfd7f0076d02d5f5e4b00e90df2b41fb5736
agartanah <vyatin2017@mail.ru>
2025-05-07 01:36:41 +0700

    fix handle errors and add favorite service addding/removing controller

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,71 +1,133 @@
-import { Request, Response } from "express";
-import { Student, Teacher } from "../models/users";
+import { NextFunction, Request, Response } from 'express';
+import { Teacher } from '../models/Teacher';
+import { Student } from '../models/Student';
+import { objectIdSchema } from '../schemas/objectIdSchema';
+import { Course } from '../models/Course';
+import { Types } from 'mongoose';
 
-const getProfile = async (req: Request, res: Response) => {
-  const { userId, role } = req.body.user;
+const getProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { userId, role } = req;
 
-  if (!userId) {
-    res.status(400).send("Пользователь не найден");
-    return;
-  }
-
-  let user;
-
-  switch (role) {
-    case "student":
-      user = await Student.findById(userId);
-      break;
-    case "teacher":
-      user = await Teacher.findById(userId);
-      break;
-    default:
-      res.status(400).send("Неверная роль");
+    if (!userId) {
+      res.status(400).json({ error: 'ID Пользователя не найдено' });
       return;
-  }
+    }
 
-  if (!user) {
-    res.status(400).send("Пользователь не найден");
-  }
+    let user;
 
-  res.json({
-    firstName: user?.firstName,
-    lastName: user?.lastName,
-    login: user?.login,
-    role,
-  });
+    switch (role) {
+      case 'student':
+        user = await Student.findById(userId);
+        break;
+      case 'teacher':
+        user = await Teacher.findById(userId);
+        break;
+      default:
+        res.status(400).json({ error: 'Неверная роль' });
+        return;
+    }
+
+    if (!user) {
+      res.status(404).json({ error: 'Пользователь не найден' });
+      return;
+    }
+
+    res.status(200).json({
+      data: {
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        login: user?.login,
+        role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteProfile = async (req: Request, res: Response) => {
-  const userId = req.userId;
-  const role = req.role;
+const changeFavoriteCourse = (add: boolean = true) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId, role } = req;
+      const course_id = objectIdSchema.parse(req.body.course_id);
 
-  if (!userId) {
-    res.status(400).send("Пользователь не найден");
-    return;
-  }
+      if (!userId) {
+        res.status(400).json({ error: 'ID Пользователя не был найден' });
+        return;
+      }
 
-  let delUser;
+      if (!role || role !== 'student') {
+        res.status(400).json({ error: 'Неверная роль' });
+      }
 
-  switch (role) {
-    case "student":
-      delUser = await Student.findByIdAndDelete(userId);
+      if (!(await Course.findById(course_id))) {
+        res.status(404).json({ error: 'Курс не найден' });
+        return;
+      }
 
-      break;
-    case "teacher":
-      delUser = await Teacher.findByIdAndDelete(userId);
+      const student = await Student.findById(userId);
 
-      break;
-    default:
-      res.status(400).send("Неверная роль");
-      return;
-  }
+      if (!student) {
+        res.status(404).json({ error: 'Студент не найден' });
+        return;
+      }
 
-  if (!delUser) {
-    res.status(400).send("Пользователь не найден");
-    return;
-  }
+      const objectIdCourse = new Types.ObjectId(course_id);
 
-  res.status(200).json({ message: "Пользователь был успешно удалён" });
+      if (add) {
+        student.favorite_courses.push(objectIdCourse);
+      } else {
+        student.favorite_courses.pull(objectIdCourse);
+      }
+
+      student.save();
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      next(error);
+    }
+  };
 };
 
-export { getProfile, deleteProfile };
+const deleteProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { userId, role } = req;
+
+    if (!userId) {
+      res.status(400).json({ error: 'ID Пользователя не найден' });
+      return;
+    }
+
+    let delUser;
+
+    switch (role) {
+      case 'student':
+        delUser = await Student.findByIdAndDelete(userId);
+
+        break;
+      case 'teacher':
+        delUser = await Teacher.findByIdAndDelete(userId);
+
+        break;
+      default:
+        res.status(400).json({ error: 'Неверная роль' });
+        return;
+    }
+
+    if (!delUser) {
+      res.status(400).json({ error: 'Пользователь не найден' });
+      return;
+    }
+
+    res.status(204).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getProfile, changeFavoriteCourse, deleteProfile };
