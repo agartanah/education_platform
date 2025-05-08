66a9e5d000e94f4d7f02ab5d6e80d698175857ca
agartanah <vyatin2017@mail.ru>
2025-05-08 19:41:10 +0700

    delete course id schema

--- src/schemas/courseSchema.ts
+++ src/schemas/courseSchema.ts
@@ -1,10 +1,6 @@
 import { z } from 'zod';
 import { objectIdSchema } from './objectIdSchema';
 
-export const courseIdSchema = z.object({
-  course_id: objectIdSchema,
-});
-
 export const createCourseSchema = z.object({
   title: z.string().min(1),
   description: z.string().min(1),
@@ -20,4 +16,3 @@ export const updateCourseSchema = createCourseSchema.partial();
 
 export type CreateCourseDto = z.infer<typeof createCourseSchema>;
 export type UpdateCourseDto = z.infer<typeof updateCourseSchema>;
-export type CourseIdDto = z.infer<typeof courseIdSchema>;
