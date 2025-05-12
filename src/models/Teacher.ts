8bc1b19b8973dd0409a50ac688c04efa2abb410e
agartanah <vyatin2017@mail.ru>
2025-05-12 22:35:06 +0700

    add false selecting for login and password

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -3,8 +3,8 @@ import { Schema, model } from 'mongoose';
 const TeacherSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  login: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  login: { type: String, required: true, unique: true, select: false },
+  password: { type: String, required: true, select: false },
 });
 
 const Teacher = model('Teacher', TeacherSchema);
