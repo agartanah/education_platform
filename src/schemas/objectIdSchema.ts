f19e868349923caa44fefd795c1ffc2b0deb7133
agartanah <vyatin2017@mail.ru>
2025-06-24 23:06:50 +0700

    refactor: delete old files

--- src/schemas/objectIdSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import mongoose from 'mongoose';
-
-export const objectIdSchema = z
-  .string()
-  .refine((val) => mongoose.Types.ObjectId.isValid(val), {
-    message: 'Неверный формат MongoDB ObjectId',
-  });
