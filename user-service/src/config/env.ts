5920790b19f6be1358beeef74e9f954bc4f4c759
agartanah <vyatin2017@mail.ru>
2025-06-25 02:11:34 +0700

    fix: references for mongodb and rabbitmq

--- user-service/src/config/env.ts
+++ user-service/src/config/env.ts
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/education',
+  mongoUri:
+    process.env.MONGO_URI ||
+    'mongodb://admin:password@mongodb:27017/education?authSource=admin',
   port: process.env.PORT || '3001',
   jwt_secret: process.env.JWT_SECRET || 'secret',
-  rabbitmqUrl: process.env.RABBITMQ_URL || 'amqp://localhost:5672',
+  rabbitmqUrl:
+    process.env.RABBITMQ_URL || 'amqp://admin:password@rabbitmq:5672',
 };
